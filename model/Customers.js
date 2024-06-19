b74a966ed56cbd54c0ad0965c46462b5e2d7f301
zbytes2227 <zbytes0@gmail.com>
2024-06-19 12:08:20 +0530

    first commit

---
+++ model/Customers.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+// mongoose.set('strictQuery', true);
+
+const CustomerSchema = new mongoose.Schema(
+  {
+    CustomerID: { type: String, required: true, unique: true },
+    CustomerName: { type: String },
+    CustomerPhone: { type: String, required: true, unique: true },
+    CustomerEmail: { type: String, required: true, unique: true },
+    CustomerBranch: { type: String, required: true},
+    CustomerCollege: { type: String},
+  },
+  { collection: "all-Customers" },
+  { timestamps: true }
+);
+
+mongoose.models = {};
+const Customers = mongoose.model("Customers", CustomerSchema);
+module.exports = Customers;
