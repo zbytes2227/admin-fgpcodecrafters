db29641cf340b44787e7584230939ca079022b87
zbytes2227 <zbytes0@gmail.com>
2024-06-21 17:10:20 +0530

    community link update

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -4,11 +4,7 @@ const handler = async (req, res) => {
   if (req.method === 'GET') {
     try {
       res.setHeader('Set-Cookie', serialize('admin_access_token', '', {
-        httpOnly: true,
-        sameSite: 'strict',
-        expires: new Date(0),
         path: '/',
-        secure: true,
       }));
       return res.json({ success: true, msg: 'Logout Successful' });
     } catch (err) {
