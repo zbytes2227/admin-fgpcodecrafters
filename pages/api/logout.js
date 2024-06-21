b30d381a9d4d1fcb6dcf31cc7295fe2ebfeacde4
zbytes2227 <zbytes0@gmail.com>
2024-06-21 17:12:47 +0530

    logout c

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -3,10 +3,8 @@ import { serialize } from 'cookie';
 const handler = async (req, res) => {
   if (req.method === 'GET') {
     try {
-      res.setHeader('Set-Cookie', serialize('admin_access_token', '', {
-        path: '/',
-      }));
-      return res.json({ success: true, msg: 'Logout Successful' });
+     
+      return  res.setHeader('Set-Cookie', serialize('admin_access_token', '', {path: '/',})).json({ success: true, msg: 'Logout Successful' });
     } catch (err) {
       console.error(err);
       return res.status(500).json({ success: false, msg: 'Internal Server Error' });
